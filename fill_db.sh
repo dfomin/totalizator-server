addfd4abab33e1da5c998ef71748695dca9dd7f9
Dmitry Fomin <fomin.dm@gmail.com>
2022-04-26 23:55:46 +0300

    New results

--- fill_db.sh
+++ fill_db.sh
@@ -88,3 +88,14 @@ curl -X POST "http://0.0.0.0:8000/vote?user_id=6&match_id=8&score1=3&score2=1"
 
 curl -X POST "http://0.0.0.0:8000/add_match_result?match_id=7&score1=0&score2=0"
 curl -X POST "http://0.0.0.0:8000/add_match_result?match_id=8&score1=3&score2=3"
+
+curl -X POST "http://0.0.0.0:8000/add_match?competition_id=1&name=mancity_real"
+
+curl -X POST "http://0.0.0.0:8000/vote?user_id=1&match_id=9&score1=3&score2=1"
+curl -X POST "http://0.0.0.0:8000/vote?user_id=2&match_id=9&score1=2&score2=1"
+curl -X POST "http://0.0.0.0:8000/vote?user_id=3&match_id=9&score1=2&score2=1"
+curl -X POST "http://0.0.0.0:8000/vote?user_id=4&match_id=9&score1=2&score2=1"
+curl -X POST "http://0.0.0.0:8000/vote?user_id=5&match_id=9&score1=2&score2=2"
+curl -X POST "http://0.0.0.0:8000/vote?user_id=6&match_id=9&score1=1&score2=0"
+
+curl -X POST "http://0.0.0.0:8000/add_match_result?match_id=9&score1=4&score2=3"
